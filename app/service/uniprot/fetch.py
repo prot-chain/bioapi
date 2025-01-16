b4c3226fe9184c04cde3269adb75b8adbb87eb34
isongjosiah <isongjosiah@gmail.com>
2025-01-16 17:31:03 +0100

    chore: improve parsing functionality to important information

--- app/service/uniprot/fetch.py
+++ app/service/uniprot/fetch.py
@@ -8,7 +8,7 @@ class UniprotFetchService:
 
     BASE_URL = "https://rest.uniprot.org/uniprotkb"
 
-    async def fetch_protein_data(self, protein_id: str) -> dict:
+    async def fetch_protein_data(self, protein_id: str, format: str="json") -> dict:
         """
         Fetch raw protein data from the PDB API.
 
@@ -19,12 +19,15 @@ class UniprotFetchService:
             dict: Raw protein data.
         """
         async with AsyncClient() as client:
-            response = await client.get(f"{self.BASE_URL}/{protein_id}?format=json")
+            response = await client.get(
+                f"{self.BASE_URL}/{protein_id}?format={format}")
             if response.status_code != 200:
-                raise Exception(f"Failed to fetch protein data for ID {protein_id}")
+                raise Exception(
+                    f"Failed to fetch protein data for ID {protein_id}")
+            print(response.json()) 
             return response.json()
 
-    def parse_protein_data(self, raw_data: dict) -> dict:
+    def parse_protein_data(self, data: dict) -> dict:
         """
         Parse raw protein data into a structured format.
 
@@ -34,10 +37,71 @@ class UniprotFetchService:
         Returns:
             dict: Parsed protein data.
         """
-        return {
-            "id": raw_data.get("primaryAccession"),
-            "name": raw_data.get("protein", {}).get("recommendedName", {}).get("fullName", {}).get("value"),
-            "organism": raw_data.get("organism", {}).get("scientificName"),
-            "sequence": raw_data.get("sequence", {}).get("value"),
-            "length": raw_data.get("sequence", {}).get("length"),
+
+        parsed_data = {
+            "primary_accesion": data.get("primaryAccession"),
+            "recommended_name": data.get("proteinDescription", {}).get(
+                "recommendedName", {}).get("fullName", {}).get("value"),
+            "organism": {
+                "scientific_name": data.get("organism", {}).get(
+                    "scientificName"),
+                "common_name": data.get("organism", {}).get("commonName"),
+            },
+            "entry_audit": {
+                "First Public Date": data.get("entryAudit", {}).get(
+                    "firstPublicDate"),
+                "Last Annotation Update Date": data.get("entryAudit", {}).get(
+                    "lastAnnotationUpdateDate"),
+                "Sequence Version": data.get("entryAudit", {}).get(
+                    "sequenceVersion"),
+                "Entry Version": data.get("entryAudit", {}).get(
+                    "entryVersion"),
+            },
+            "functions": [
+                comment.get("texts", [{}])[0].get("value")
+                for comment in data.get("comments", [])
+                if comment.get("commentType") == "FUNCTION"
+            ],
+            "subunit_structure": [
+                comment.get("texts", [{}])[0].get("value")
+                for comment in data.get("comments", [])
+                if comment.get("commentType") == "SUBUNIT"
+            ],
+            "subcellular_locations": [
+                loc.get("location", {}).get("value")
+                for comment in data.get("comments", [])
+                if comment.get("commentType") == "SUBCELLULAR LOCATION"
+                for loc in comment.get("subcellularLocations", [])
+            ],
+            "Disease Associations": [
+                {
+                    "Disease Name": comment.get("disease", {}).get("description"),
+                    "Acronym": comment.get("disease", {}).get("acronym"),
+                    "Cross Reference": comment.get("disease", {}).get(
+                        "diseaseCrossReference", {}).get("database")
+                }
+                for comment in data.get("comments", [])
+                if comment.get("commentType") == "DISEASE"
+            ],
+            "isoforms": [
+                {
+                    "Isoform Name": isoform.get("name", {}).get("value"),
+                    "Sequence Status": isoform.get("isoformSequenceStatus")
+                }
+                for comment in data.get("comments", [])
+                if comment.get("commentType") == "ALTERNATIVE PRODUCTS"
+                for isoform in comment.get("isoforms", [])
+            ],
+            "features": [
+                {
+                    "Type": feature.get("type"),
+                    "Location": f"{feature.get('location', {}).get('start', {}).get(
+                                'value')} - {feature.get('location', {}).get(
+                                    'end', {}).get('value')}",
+                    "Description": feature.get("description")
+                }
+                for feature in data.get("features", [])
+            ],
         }
+
+        return parsed_data
