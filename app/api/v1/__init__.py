eedfc8935fa59dae9eecefca35b52dc82acd8421
xavier <kanuxavier@gmail.com>
2025-03-23 16:12:23 +0900

    Add workflow management API to BioAPI

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,6 +1,8 @@
 from .endpoints import protein
+from .endpoints import workflow
 
 from fastapi import APIRouter
 
 router = APIRouter()
 router.include_router(protein.router, prefix="/protein", tags=["uniprot"])
+router.include_router(workflow.router, prefix="/workflows", tags=["workflows"])
